5c6f43f041d0d8e8dfaddbfa29786f0409cf4c04
RobRibProg <rribeiro1@byupathway.edu>
2025-09-03 18:07:28 -0300

    NavBar first code

---
+++ scripts/navigation.js
@@ -0,0 +1,6 @@
+const navButtom = document.querySelector("#ham-btn");
+
+//Toggle the show class off and on
+navButtom.addEventListener("click", () => {
+    navButtom.classList.toggle("show");
+});
