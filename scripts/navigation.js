ff898bc8a3658630d85bfdc31ebba1536dd1d045
RobRibProg <rribeiro1@byupathway.edu>
2025-09-05 21:45:54 -0300

    First week activities completed

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,6 +1,21 @@
+
 const navButtom = document.querySelector("#ham-btn");
 
 //Toggle the show class off and on
 navButtom.addEventListener("click", () => {
     navButtom.classList.toggle("show");
+    navBar.classList.toggle("show");
 });
+
+/************************NAVBAR************************/
+
+const navBar = document.querySelector("#nav-bar");
+
+    const name = "robson";
+    let lastName = "Ribeiro"
+    var age = 25
+    
+function teste(){
+    console.log(age)
+}
+t
