081d8607e0993403b6486107abd51b46cd5f21c5
RobRibProg <rribeiro1@byupathway.edu>
2025-10-13 21:39:41 -0300

    .

---
+++ finalProject/scripts/recipes.js
@@ -0,0 +1,49 @@
+const container = document.getElementById("recipe-container");
+const preBtn = document.getElementById("pre-btn");
+const postBtn = document.getElementById("post-btn");
+
+async function getRecipes(foods) {
+    for (let food of foods) {
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${food}`);
+        const data = await response.json();
+
+        if (data.meals) {
+            data.meals.forEach(meal => {
+                const card = document.createElement("div");
+                card.classList.add("card");
+
+                card.innerHTML = `
+                <img src="${meal.strMealThumb}" alt="${meal.strMeal}">
+                <div class="card-content">
+                    <h2>${meal.strMeal}</h2>
+                    <p><strong>Categorie:</strong> ${meal.strCategory}</p>
+                    <p><strong>Origin:</strong> ${meal.strArea}</p>
+                    <p>${meal.strInstructions.substring(0, 100)}...</p>
+                    <a href="${meal.strYoutube}" target="_blank" style="color:#C80036;">See video</a>
+                </div>
+                `
+                container.appendChild(card);
+            })
+        }
+    }
+}
+const preWorkout = ["oat", "banana", "smoothie", "egg", "avocado"];
+const postWorkout = ["chicken", "salmon", "rice", "pasta", "protein"];
+
+getRecipes(preWorkout);
+
+preBtn.addEventListener("click", () => {
+    preBtn.classList.add("active");
+    postBtn.classList.remove("active");
+    container.innerHTML = "";
+    getRecipes(preWorkout);
+});
+
+postBtn.addEventListener("click", () => {
+    postBtn.classList.add("active");
+    preBtn.classList.remove("active");
+    container.innerHTML = "";
+    getRecipes(postWorkout);
+});
+
+
